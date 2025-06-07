666a1c95c59aa1b84635b5076122e9924f6ee79a
fernandobn <154335390+fernandobn@users.noreply.github.com>
2025-06-07 15:03:53 -0500

    menu
    vista de la pantalla de inicio

---
+++ fitday/lib/screens/menu_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/imagenfinal.png',
+            fit: BoxFit.cover,
+            filterQuality: FilterQuality.high,
+            color: Colors.black.withOpacity(0.2),
+            colorBlendMode: BlendMode.darken,
+          ),
+          Positioned(
+            bottom: 200,
+            left: 20,
+            right: 20,
+            child: Column(
+              children: [
+                const Text(
+                  'LA ACCIÓN ES LA CLAVE DE TODO ÉXITO',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.limeAccent,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 12,
+                    ),
+                  ),
+                  onPressed: () {
+                    // Navigator.pushNamed(context, '/home');
+                  },
+                  child: const Text(
+                    'Empezar Ahora',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
